46905d6337b966184781bed1239e6856bb4968ba
Camila Augusta dos Santos <camilaaugustasantos@yahoo.com.br>
2021-06-19 18:38:25 -0300

    MultiplicacaoSimples

--- README.md
+++ README.md
@@ -59,5 +59,26 @@ public class MultiplicacaoSimples {
     System.out.println("PROD = " + resultado);
   }
 }
+
+//SOLUCAO 2
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class MultiplicacaoSimples {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int valorUm = Integer.parseInt(st.nextToken());
+
+    st = new StringTokenizer(br.readLine());
+    int valorDois = Integer.parseInt(st.nextToken());
+   
+    System.out.println("PROD = " + (valorUm * valorDois));
+  }
+}
 ```
 
--- java-multiplicacao-simples.java
+++ java-multiplicacao-simples.java
@@ -1,3 +1,5 @@
+//SOLUCAO 1
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -16,5 +18,26 @@ public class MultiplicacaoSimples {
     int resultado = valorUm * valorDois;
     
     System.out.println("PROD = " + resultado);
+  }
+}
+
+//SOLUCAO 2
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class MultiplicacaoSimples {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int valorUm = Integer.parseInt(st.nextToken());
+
+    st = new StringTokenizer(br.readLine());
+    int valorDois = Integer.parseInt(st.nextToken());
+   
+    System.out.println("PROD = " + (valorUm * valorDois));
   }
 }
